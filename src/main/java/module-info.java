78ee57fff8e7d360a68d05c261a34304a96c6128
RobeOrtiz <robeortizlopez@gmail.com>
2024-04-26 10:23:18 +0200

    incluido funcionamiento descargarPDF

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module es.finalPrimerCurso.finalPrimerCurso {
     requires javafx.controls;
     requires javafx.fxml;
+	requires org.apache.pdfbox;
 
     opens es.finalPrimerCurso.finalPrimerCurso to javafx.fxml;
     exports es.finalPrimerCurso.finalPrimerCurso;
